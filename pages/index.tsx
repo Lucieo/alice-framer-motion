b50bc0d3c96d0f432c3d48930eb3ed7dfb1348a7
lucieo <luciecamilleory@gmail.com>
2020-11-25 15:39:36 +0100

    whileHover with parent child variants

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,25 @@ import Link from 'next/link'
 import { book } from 'data/alice'
 import { motion } from 'framer-motion'
 
+const homePageWrapperVariant = {
+  hidden: { opacity: 0, y: '-100px' },
+  visible: {
+    opacity: 1,
+    y: 0,
+    transition: { duration: 1.5, when: 'beforeChildren', staggerChildren: 1 },
+  },
+}
+
 const chapterCardVariant = {
+  hidden: {
+    opacity: 0,
+  },
+  visible: {
+    opacity: 1,
+    transition: {
+      duration: 1.5,
+    },
+  },
   hover: {
     scale: 1.1,
     transition: { type: 'spring', bounce: 0.6 },
@@ -20,38 +38,38 @@ export default function AliceFramerMotion() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <motion.div
-        initial={{ opacity: 0, y: '-100px' }}
-        animate={{ opacity: 1, y: 0 }}
-        transition={{ duration: 1.5 }}
+        variants={homePageWrapperVariant}
+        initial="hidden"
+        animate="visible"
       >
         <Text textAlign="center" fontSize="3xl" p={4}>
           {book.title}
         </Text>
+        <Flex paddingY={10}>
+          {book.chapters.map((chapter, index) => (
+            <Box p={4} key={index}>
+              <motion.div whileHover="hover" variants={chapterCardVariant}>
+                <Link href={`/chapter/${index + 1}`}>
+                  <Square
+                    size="200px"
+                    backgroundImage={`url('/alice/${chapter.illustration}')`}
+                    backgroundSize="cover"
+                    backgroundPosition="center"
+                    borderRadius="lg"
+                    boxShadow="2xl"
+                    border="2px solid lightgray"
+                    cursor="pointer"
+                  >
+                    <Text fontSize="50px" fontWeight="bold" color="#D4AF37">
+                      {chapter.romanIndex}
+                    </Text>
+                  </Square>
+                </Link>
+              </motion.div>
+            </Box>
+          ))}
+        </Flex>
       </motion.div>
-      <Flex paddingY={10}>
-        {book.chapters.map((chapter, index) => (
-          <Box p={4} key={index}>
-            <motion.div whileHover="hover" variants={chapterCardVariant}>
-              <Link href={`/chapter/${index + 1}`}>
-                <Square
-                  size="200px"
-                  backgroundImage={`url('/alice/${chapter.illustration}')`}
-                  backgroundSize="cover"
-                  backgroundPosition="center"
-                  borderRadius="lg"
-                  boxShadow="2xl"
-                  border="2px solid lightgray"
-                  cursor="pointer"
-                >
-                  <Text fontSize="50px" fontWeight="bold" color="#D4AF37">
-                    {chapter.romanIndex}
-                  </Text>
-                </Square>
-              </Link>
-            </motion.div>
-          </Box>
-        ))}
-      </Flex>
     </Center>
   )
 }
